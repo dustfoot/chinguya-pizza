fd354d2380d6e4ee03c15df0ded76bae22be890c
dustfoot <dustfoot3668@gmail.com>
2019-11-10 14:54:01 +0300

    starting js and jquery

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+class Pizza {
+   constructor(type, size, crust, toppings, quantity, delivery) {
+      this.type = type;
+      this.size = size;
+      this.crust = crust;
+      this.toppings = toppings;
+      this.quantity = quantity;
+      this.delivery = delivery;
+   }
+   
