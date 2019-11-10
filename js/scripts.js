b4570cb0c1d7791ebe78811b433527bea5626a83
dustfoot <dustfoot3668@gmail.com>
2019-11-10 14:58:33 +0300

    adding the crustPrice if statements

--- js/scripts.js
+++ js/scripts.js
@@ -15,64 +15,25 @@ class Pizza {
          return this.price;
       }
    }
-   priceBySize(){
-      if (this.size == "Large"){
-        return 1500;
-      } else if (this.size == "Medium"){
-        return 1000;
+   priceBySize() {
+      if (this.size == "Large") {
+         return 1500;
+      } else if (this.size == "Medium") {
+         return 1000;
       } else {
-        return 500;
+         return 500;
       }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+   }
+   crustPrice() {
+      if (this.crust == "Tripple Cheese Crust") {
+         return 400;
+      } else if (this.crust == "Double Cheese Crust") {
+         return 300;
+      } else if (this.crust == "Puff Pastry") {
+         return 200;
+      } else if (this.crust == "Cheesy Crust") {
+         return 150;
+      } else {
+         return 0;
+      }
+   }
